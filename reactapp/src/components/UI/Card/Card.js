69ce49b1733fe4b8bf8bb204efb6b7f5179861f5
Iamneo <techadmin@iamneo.ai>
2023-08-05 16:13:19 +0000

    Backup Commit

--- reactapp/src/components/UI/Card/Card.js
+++ reactapp/src/components/UI/Card/Card.js
@@ -28,8 +28,25 @@ const Card = ({
     }
     if (clicked === answer) correctAnswerMarkUpdate(attempt + 1);
   };
-  const norm = "bg-orange-300 px-2 py-1 rounded hover:bg-orange-400";
-  const disabled = "bg-orange-300 px-2 py-1 rounded disabled:opacity-50";
+  const norm = {
+    backgroundColor: '#FFA500', // bg-orange-300
+    paddingLeft: '2px',        // px-2
+    paddingRight: '2px',       // px-2
+    paddingTop: '1px',         // py-1
+    paddingBottom: '1px',      // py-1
+    borderRadius: '0.25rem',   // rounded
+    ':hover': {                // hover:bg-orange-400
+      backgroundColor: '#FF8C00',
+    }}
+  const disabled = {
+    backgroundColor: '#FFA500',    // bg-orange-300
+    paddingLeft: '2px',            // px-2
+    paddingRight: '2px',           // px-2
+    paddingTop: '1px',             // py-1
+    paddingBottom: '1px',          // py-1
+    borderRadius: '0.25rem',       // rounded
+    opacity: '0.5',                // disabled:opacity-50
+  }
   return (
     <div>
       <h4 className="pt-3 pb-1">{question}</h4>
