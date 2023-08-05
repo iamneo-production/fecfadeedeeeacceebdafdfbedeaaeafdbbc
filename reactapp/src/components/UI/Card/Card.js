957af9a6fc88b1d1c4674654adfc802bce9fe69c
Iamneo <techadmin@iamneo.ai>
2023-08-05 16:33:19 +0000

    Backup Commit

--- reactapp/src/components/UI/Card/Card.js
+++ reactapp/src/components/UI/Card/Card.js
@@ -16,7 +16,7 @@ const Card = ({
     options.option1,
     options.option2,
     options.option3,
-    options.option4,
+    options.option4
   ];
   const [marked, setMarked] = useState(false);
   const click = (props,clicked) => {
